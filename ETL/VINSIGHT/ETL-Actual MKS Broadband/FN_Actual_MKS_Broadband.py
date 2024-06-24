81ef17da6edd44596a26842230e4acd0ddf5dd59
ruzlim <narut.lim@gmail.com>
2024-06-24 11:41:30 +0700

    sync

--- ETL/VINSIGHT/ETL-Actual MKS Broadband/FN_Actual_MKS_Broadband.py
+++ ETL/VINSIGHT/ETL-Actual MKS Broadband/FN_Actual_MKS_Broadband.py
@@ -27,18 +27,19 @@ AKPIPRD_db = config['AKPIPRD']['db']
 AKPIPRD_host = config['AKPIPRD']['host']
 AKPIPRD_port = config['AKPIPRD']['port']
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
 
-def src_update_to_fact(v_last_mth_fct, v_last_mth_src, v_target_schema, v_target_table, v_sql_update_fact):
 
-    last_mth_fct = v_last_mth_fct
-    last_mth_src = v_last_mth_src
+def src_update_to_fact(v_mth_end_fct, v_target_schema, v_target_table, v_sql_update_fact):
+
+    # Get : Parameter
+    mth_end_fct = v_mth_end_fct
     target_schema = v_target_schema
     target_table = v_target_table
     sql_update_fact = v_sql_update_fact
+    v_param = dict(mth_end_fct=mth_end_fct)
 
-    v_param = dict(last_mth_fct=last_mth_fct, last_mth_src=last_mth_src)
-
-    txt_param_input = f'\nParam input...\n   -> last_mth_fct\n   -> last_mth_src\n   -> target_schema\n   -> target_table\n   -> sql_update_fact'
+    txt_param_input = f'\nParam input...\n   -> mth_end_fct\n   -> target_schema\n   -> target_table\n   -> sql_update_fact'
 
     # Read : SQL file
     with open(f'SQL/{sql_update_fact}', 'r') as sql_file:
@@ -76,7 +77,7 @@ def src_update_to_fact(v_last_mth_fct, v_last_mth_src, v_target_schema, v_target
         # Delete
         tgt_cur.execute(f"""
             DELETE {target_schema}.{target_table} 
-            WHERE TM_KEY_MTH > {last_mth_fct}
+            WHERE TM_KEY_MTH > {mth_end_fct}
         """)
         print(f'\n   -> DELETE : "{target_table}" : Done !')
         
@@ -104,7 +105,77 @@ def src_update_to_fact(v_last_mth_fct, v_last_mth_src, v_target_schema, v_target
 
         print(f'\nJob Done !!!')
 
-    # return print(txt_param_input)
+
+    return print(txt_param_input)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+def mockup_to_fact(v_mth_end_fct, v_target_schema, v_target_table, v_sql_mockup_fact):
+
+    # Get : Parameter
+    mth_end_fct = v_mth_end_fct
+    target_schema = v_target_schema
+    target_table = v_target_table
+    sql_mockup_fact = v_sql_mockup_fact
+    v_param = dict(mth_end_fct=mth_end_fct)
+    
+    txt_param_input = f'\nParam input...\n   -> mth_end_fct\n   -> target_schema\n   -> target_table\n   -> sql_mockup_fact'
+
+    # Read : SQL file
+    with open(f'SQL/{sql_mockup_fact}', 'r') as sql_file:
+        queries = sql_file.read().split(';')
+        query = queries[0].strip()
+        sql_file.close()
+        
+    # Connect : AKPIPRD
+    tgt_dsn = f'{AKPIPRD_user}/{AKPIPRD_pwd}@{AKPIPRD_host}:{AKPIPRD_port}/{AKPIPRD_db}'
+    tgt_conn = oracledb.connect(tgt_dsn)
+    print(f'\n{AKPIPRD_db} : Connected')
+    tgt_cur = tgt_conn.cursor()
+
+
+    try:
+        print(f'\nProcessing...')
+
+        # Delete
+        tgt_cur.execute(f"""
+            DELETE {target_schema}.{target_table} 
+            WHERE TM_KEY_MTH > {mth_end_fct}
+        """)
+        print(f'\n   -> DELETE : "{target_table}" : Done !')
+        
+        # Insert
+        tgt_cur.execute(query, v_param)
+        # rows = tgt_cur.fetchall()
+        # mock_df = pd.DataFrame.from_records(rows, columns=[x[0] for x in tgt_cur.description])
+        # print(f'\n   -> mock_df : {mock_df.shape[0]} rows, {mock_df.shape[1]} columns') 
+        print(f'\n   -> INSERT : "{target_table}" : Done !')
+
+        tgt_cur.close()
+        tgt_conn.commit()
+
+        # Insert
+        # tgt_cur.executemany(f"""
+        #     INSERT INTO {target_schema}.{target_table}
+        #     (TM_KEY_YR, TM_KEY_MTH, TRUE_TM_KEY_WK, TM_KEY_DAY, METRIC_CD, METRIC_NAME, COMP_CD, VERSION, AREA_NO, AREA_TYPE, AREA_CD, AREA_NAME, METRIC_VALUE, AGG_TYPE, FREQUENCY, REMARK) 
+        #     VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16)
+        # """, rows)
+        
+
+    except oracledb.DatabaseError as e:
+        print(f'\nError with Oracle : {e}')
+
+
+    finally:
+        tgt_conn.close()
+        print(f'\n{AKPIPRD_db} : Disconnected')
+        print(f'\nJob Done !!!')
+
+
+    return print(txt_param_input)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 def main():
---
+++ ETL/VINSIGHT/ETL-Actual MKS Broadband/SQL/Mock-FCT_BROADBAND_MKS.sql
@@ -0,0 +1,51 @@
+
+/*** Mockup last actual data for VINSIGHT display to "AUTOKPI.FCT_BROADBAND_MKS" ***/
+
+-----------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO AUTOKPI.FCT_BROADBAND_MKS (TM_KEY_YR, TM_KEY_MTH, TRUE_TM_KEY_WK, TM_KEY_DAY, METRIC_CD, METRIC_NAME, COMP_CD, VERSION, AREA_NO, AREA_TYPE, AREA_CD, AREA_NAME, METRIC_VALUE, AGG_TYPE, FREQUENCY, REMARK)
+
+-----------------------------------------------------------------------------------------------------------------
+
+WITH W_PARAM AS
+(
+	SELECT :mth_end_fct AS V_MTH_END_FCT
+		, TO_NUMBER(TO_CHAR(CURRENT_DATE + 7, 'YYYYMM')) AS V_PREP_MTH_NEXT_WK
+	FROM DUAL
+) -->> W_PARAM
+-----------------------------------------------------------------------------------------------------------------------
+
+, W_PREP_PERIOD AS 
+(
+	SELECT TM_KEY_YR, TM_KEY_MTH, TRUE_TM_KEY_WK, TM_KEY_DAY
+	FROM AUTOKPI.DIM_TIME NOLOCK
+	WHERE TM_KEY_MTH > (SELECT V_MTH_END_FCT FROM W_PARAM)
+	AND TM_KEY_MTH <= (SELECT V_PREP_MTH_NEXT_WK FROM W_PARAM)
+) -->> W_PREP_PERIOD
+-----------------------------------------------------------------------------------------------------------------------
+
+, W_RAW_BROADBAND_MKS_LAST_MTH_ACTUAL AS
+(
+	SELECT TM_KEY_MTH, METRIC_CD, METRIC_NAME, COMP_CD, VERSION, AREA_NO, AREA_TYPE, AREA_CD, AREA_NAME, METRIC_VALUE, AGG_TYPE, FREQUENCY
+	FROM AUTOKPI.FCT_BROADBAND_MKS NOLOCK
+	WHERE TM_KEY_MTH = (SELECT V_MTH_END_FCT FROM W_PARAM)
+	AND TM_KEY_DAY LIKE '%01'
+) -->> W_RAW_BROADBAND_MKS_LAST_MTH_ACTUAL
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
+
+-->> Output to "FCT_BROADBAND_MKS"
+
+SELECT P.TM_KEY_YR, P.TM_KEY_MTH, P.TRUE_TM_KEY_WK, P.TM_KEY_DAY
+	, METRIC_CD, METRIC_NAME, COMP_CD, VERSION, AREA_NO, AREA_TYPE, AREA_CD, AREA_NAME, METRIC_VALUE, AGG_TYPE, FREQUENCY
+	, 'Data as of : '||(SELECT V_MTH_END_FCT FROM W_PARAM) AS REMARK
+FROM W_RAW_BROADBAND_MKS_LAST_MTH_ACTUAL A
+LEFT JOIN W_PREP_PERIOD P
+	ON 1=1
+WHERE A.TM_KEY_MTH = (SELECT V_MTH_END_FCT FROM W_PARAM)
+ORDER BY TM_KEY_DAY, METRIC_CD, AREA_NO, AREA_CD
+;
+
+-->> Test
+-- AND METRIC_CD = 'VIN00020' AND AREA_CD = 'P'
